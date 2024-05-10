abd90fde8b550309f7b1ed0ced101ed8e1d48ae9
Yura Kasyan <33.yurakasyan@gmail.com>
2024-05-10 20:56:32 +0200

    add h/m 06

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,34 @@
 'use strict';
+
+class StringBuilder {
+  #initialValue;
+
+  constructor(initialValue) {
+    this.#initialValue = initialValue;
+  }
+
+  getValue() {
+    return this.#initialValue;
+  }
+
+  padEnd(str) {
+    this.#initialValue = `${this.#initialValue}${str}`;
+  }
+
+  padStart(str) {
+    this.#initialValue = `${str}${this.#initialValue}`;
+  }
+
+  padBoth(str) {
+    this.#initialValue = `${str}${this.#initialValue}${str}`;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
